2928478152bc20f971ed9d515f2b2662a8d00ea9
Nicki Richter <niri0478@colorado.edu>
2019-04-07 15:01:26 -0600

    updated

--- server.py
+++ server.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
+from youtube_transcript_api import YouTubeTranscriptApi
+NO_TRANSCRIPT_ERROR=1
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-   return 'Hello, World!'
+#send verbose errors
+#send back json
+#get video transcript text
+
+@app.route('/<video_id>')
+def model(video_id):
+	#if not request.view_args.get('video_id'):
+	try:
+		YouTubeTranscriptApi.get_transcript(video_id, languages=["en"])
+	except YouTubeTranscriptApi.CouldNotRetrieveTranscript:
+		return jsonify({'error':{'code':NO_TRANSCRIPT_ERROR, 'message':'transcript could not be retrieved'}})
+	
+	return jsonify({"success":True})
+
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', debug=True)
