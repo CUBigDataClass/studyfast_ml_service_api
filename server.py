74a862f0e5030ec083592a55228b700dd89041c9
Carl Cortright <carl@carlcortright.com>
2019-04-09 14:47:25 -0600

    add logging

--- server.py
+++ server.py
@@ -12,14 +12,17 @@ app = Flask(__name__)
 def model(video_id):
 	search = request.args.get('search')
 	if not search:
+		app.logger.info("request failed without search parameter")
 		return jsonify({'error':{'code': INVALID_PARAMS_ERROR, 'message':'search parameter required on request'}})
 	try:
 		data = YouTubeTranscriptApi.get_transcript(video_id, languages=["en"])
 	except YouTubeTranscriptApi.CouldNotRetrieveTranscript:
+		app.logger.info(f"request failed on transcript lookup, video_id={video_id}")
 		return jsonify({'error':{'code': NO_TRANSCRIPT_ERROR, 'message':'transcript could not be retrieved'}})
 	try:
 		buckets = partition(data, search)
 	except Exception:
+		app.logger.info(f"video processing failed with exception")
 		return jsonify({'error':{'code': VIDEO_PROCESSING_ERROR, 'message':'error processing video'}})
 	payload = {
 		"video_id": video_id,
